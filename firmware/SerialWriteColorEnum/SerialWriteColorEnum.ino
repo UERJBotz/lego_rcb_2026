1ff3c8f0fcc21a67102ca7cdfbf3a62815e7cc02
pgvalle <pgvalle.nogueira@gmail.com>
2026-03-27 15:37:45 -0300

    comunicação serial arduino-esp: sensor de cor e distancia (testado)

--- firmware/SerialWriteColorEnum/SerialWriteColorEnum.ino
+++ firmware/SerialWriteColorEnum/SerialWriteColorEnum.ino
@@ -1,13 +1,27 @@
-#include "Ev3ColorSensor.h"
+// #include "Ev3ColorSensor.h"
+#include "NewPing.h"
 
-Ev3ColorSensor sensor(8, 7);
+// Ev3ColorSensor sensor(8, 7);
+NewPing sonar(9, 10, 127);
+
+void sendMsg(uint8_t value, bool color) {
+    value &= 0b01111111;
+    value |= color ? 0b10000000 : 0;
+    Serial.println(value, BIN);
+}
 
 void setup(){
     Serial.begin(115200);
-    sensor.begin();
+    // sensor.begin();
 }
 
 void loop(){
-    Ev3ColorResult color = sensor.read();
-    Serial.write(color.color);
+    // Ev3ColorResult color = sensor.read();
+    uint8_t clr = (uint8_t)(random() % 127);
+    // uint8_t clr = color.color;
+    sendMsg(clr, true);
+
+    unsigned int us = sonar.ping_median(5);
+    uint8_t dis = (uint8_t)sonar.convert_cm(us);
+    sendMsg(dis, false);
 }
